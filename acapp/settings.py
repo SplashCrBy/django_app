fac631f9479b1f250c34fbe6b5c40706166bc513
SplashCrBy <by634075568@gmail.com>
2022-03-30 17:06:23 +0800

    modify allowed

--- acapp/settings.py
+++ acapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-78c@b@ipj36t7g5-5x+y)oc0c)5zik5bkl#m55vevd^l*g51!!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["143.244.177.67"]
 
 
 # Application definition
