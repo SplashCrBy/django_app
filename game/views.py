22a20f8a809906450e8157017b1330077bebf8dd
SplashCrBy <by634075568@gmail.com>
2022-03-30 18:05:59 +0800

    update

--- game/views.py
+++ game/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    line1 = '<h1 style="text-align:center">War of Caster</h1>'
+    line2 = '<img src="https://img1.gamersky.com/image2019/02/20190228_syj_380_14/gamersky_01origin_01_201922815333FE.jpg"></img>'
+    return HttpResponse(line1+line2)
