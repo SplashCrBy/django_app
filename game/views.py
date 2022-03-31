6c7a2a07cf52f9b7e9408a528d9d7bc98253a6c1
SplashCrBy <by634075568@gmail.com>
2022-03-31 14:33:04 +0800

    create project structure

--- game/views.py
+++
@@ -1,6 +0,0 @@
-from django.http import HttpResponse
-
-def index(request):
-    line1 = '<h1 style="text-align:center">War of Caster</h1>'
-    line2 = '<img src="https://img1.gamersky.com/image2019/02/20190228_syj_380_14/gamersky_01origin_01_201922815333FE.jpg"></img>'
-    return HttpResponse(line1+line2)
