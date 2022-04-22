39c0049f578278e01ed2e384f7436e7dfdb2fa34
SplashCrBy <by634075568@gmail.com>
2022-04-22 16:32:09 +0800

    unify the size

--- game/static/js/src/playground/zbase.js
+++ game/static/js/src/playground/zbase.js
@@ -3,6 +3,7 @@ class AcGamePlayground {
         this.root = root;
         this.$playground = $(`<div class="ac-game-playground"></div>`);
         this.hide();
+        this.root.$ac_game.append(this.$playground);
         this.start();
 
     }
@@ -14,21 +15,40 @@ class AcGamePlayground {
 
 
     start() {
+        let outer = this;
+        $(window).resize(function() {
+            outer.resize();
+        });
+
+    }
 
 
+    resize() {
+        this.width = this.$playground.width();
+        this.height = this.$playground.height();
+        let unit = Math.min(this.width / 16, this.height / 9);
+        this.width = unit * 16;
+        this.height = unit * 9;
+        this.scale = this.height;
+        
+        if (this.game_map) {
+            this.game_map.resize();
+        }
     }
 
 
     show() {
         this.$playground.show();
-        this.root.$ac_game.append(this.$playground);
+        
+        this.resize();
+
         this.width = this.$playground.width();
         this.height = this.$playground.height();
         this.game_map = new GameMap(this);
         this.players = [];
-        this.players.push(new Player(this, this.width/2, this.height/2, this.height * 0.05, "white", this.height * 0.15, true));
+        this.players.push(new Player(this, this.width/2/this.scale, this.height/2/this.scale, this.height * 0.05 / this.scale, "white", this.height * 0.15 / this.scale, true));
         for(let i = 0; i < 5; i++) {
-            this.players.push(new Player(this, this.width/2, this.height/2, this.height * 0.05, this.get_random_color(), this.height *0.15, false));
+            this.players.push(new Player(this, this.width/2/this.scale, this.height/2/this.scale, this.height * 0.05/this.scale, this.get_random_color(), this.height *0.15 / this.scale, false));
         }
 
 
